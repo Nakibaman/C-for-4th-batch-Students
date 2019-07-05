4b780490dde2ba28579155fc8c443723786e8fbc
Nakibaman <nakibaman@gmail.com>
2019-07-05 11:17:15 +0600

    update 2
    update 2

---
+++ 05.07.2019/sum/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+    printf("Enter a number");
+    scanf("%d",&a);
+    printf("Enter another number");
+    scanf("%d",&b);
+    c=a+b;
+    printf("The sum is %d",c);
+    return 0;
+}
