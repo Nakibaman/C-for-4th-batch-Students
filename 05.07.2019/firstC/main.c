803efe2a682f38261b04ff6b83058fc7b1453e60
Nakibaman <nakibaman@gmail.com>
2019-07-05 11:09:42 +0600

    first class
    first class

---
+++ 05.07.2019/firstC/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,m;
+    printf("Enter a number -");
+    scanf("%d%d",&n,&m);
+    printf("You have entered %d and %d",n,m);
+    return 0;
+}
