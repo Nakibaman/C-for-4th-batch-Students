423a70c03bbae7befed1f252e1ad8bcba2cb3b62
Nakibaman <nakibaman@gmail.com>
2019-07-12 10:49:52 +0600

    update2

--- 12.07.2019/logic/main.c
+++ 12.07.2019/logic/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main()
 {
     int a,b,c;
-    a=2;
-    b=6;
-    c=-1;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
     if(a>b&&a>c)
         printf("a is biggest");
     else if(b>a&&b>c)
         printf("b is biggest");
-    else
+    else if(c>a&&c>b)
         printf("c is biggest");
+    else if(a==b && b==c)
+        printf("a,b and c are equal");
     return 0;
 }
