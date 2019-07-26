75c043935e8abd6e5b385bdab545c834455b1fc4
Nakibaman <nakibaman@gmail.com>
2019-07-26 10:21:10 +0600

    u1
    u1

---
+++ 26.07.2019/nested_loop/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j;
+    for(j=1;j<=5;j++)
+    {
+    for(i=1;i<=5;i++)
+        printf("* ");
+    printf("\n");
+    }
+    return 0;
+}
