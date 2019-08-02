5f85730432127e8cc2a302b8cdc9c43e506c23f5
Nakibaman <nakibaman@gmail.com>
2019-08-02 10:29:19 +0600

    u-1
    u-1

---
+++ 02.08.2019/full piramid/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=7;i++)
+    {
+        for(j=7-i;j>=1;j--)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
