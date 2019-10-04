c24a486e986486ea8aeb11c84f008aeb4414d396
Nakibaman <nakibaman@gmail.com>
2019-10-04 12:15:43 +0600

    u-3
    u-3

--- 04.10.2019/FileProcessing/main.c
+++ 04.10.2019/FileProcessing/main.c
@@ -4,12 +4,21 @@
 int main()
 {
     FILE *p;
-    int marks[5]={0};
-    int i;
+    int marks[20]={0};
+    int i,l;
+    char c;
+    l=0;
     p=fopen("test.txt","r");
-    for(i=0;i<=4;i++)
+    for (c = getc(p); c != EOF; c = getc(p))
+        if (c == ' ')
+            l = l + 1;
+
+    p=fopen("test.txt","r");
+    for(i=0;i<=l;i++)
+    {
         fscanf(p,"%d",&marks[i]);
-    for(i=0;i<=4;i++)
+    }
+    for(i=0;i<l;i++)
         printf("%d ",marks[i]);
     fclose(p);
     return 0;
